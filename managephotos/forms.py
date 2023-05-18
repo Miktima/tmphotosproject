b1999dd24dd098ac261a33d47491df00f83328c8
mtimofeev <m.timofeev@ria.ru>
2023-05-18 21:23:07 +0300

    list & add photos

--- managephotos/forms.py
+++ managephotos/forms.py
@@ -7,6 +7,15 @@ class PhotoForm(forms.ModelForm):
 	class Meta:
 		model = Photo
 		fields = ["src", "src_min", "star", "genre", "title", "place"]
+		CHOICES = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),]
+		labels = {
+			'src': 'The path to the hires photo',
+			'src_min': 'The path to the thumbnail',
+			"star": 'The rank (priority) of the photo'
+		}
+		widgets = {
+			'star': forms.RadioSelect(choices=CHOICES),
+		}
 
 class KeywordsForm(forms.Form):
 	keywords_bulk = forms.CharField(max_length=250, help_text="comma separated keywords", 
