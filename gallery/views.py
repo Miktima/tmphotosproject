4ddd407543a447d8fa275b0db5aa1c0abcb62418
mtimofeev <m.timofeev@ria.ru>
2023-06-01 21:22:52 +0300

    home initial

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from managephotos.models import Genre, Photo
+import random
 
 def home(request):
-    return render(request, 'gallery/index.html')
+    # Select genres
+    genre_ins = Genre.objects.order_by("pk").all()
+    # Select photo randomly
+    random.seed()
+    photo_ins = Photo.objects.order_by("pk").all()
+    pk_list = []
+    weight_list = []
+    for ph in photo_ins:
+        pk_list.append(ph.pk)
+        weight_list.append(ph.star)
+    choice_list = random.choices(pk_list, weights=weight_list, k=1)
+    photo_ch = Photo.objects.get(pk=choice_list[0])
+    context = {
+        "genre": genre_ins,
+        "rnd_photo": photo_ch 
+    }
+    return render(request, 'gallery/index.html', context)
